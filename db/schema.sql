a617f6f22e828ff0013ee94a3ceb15ef585f4ec8
rasputinforever <erik.justin.portillo@gmail.com>
2021-02-13 21:01:11 -0800

    db files created, set

---
+++ db/schema.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Big Mac', FALSE);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Whopper', FALSE);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Jumbo Jack', FALSE);
